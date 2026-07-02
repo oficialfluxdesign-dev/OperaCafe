5512a9792efef96da14d9d3f8099de2a97890e09
oficialfluxdesign-dev <oficialfluxdesign@gmail.com>
2026-07-02 19:56:33 -0300

    Eventos page responsive

--- src/components/HeroEventos.jsx
+++ src/components/HeroEventos.jsx
@@ -17,8 +17,8 @@ const buttonAnim = {
 
 export default function HeroEventos() {
   return (
-    <section className="relative h-screen overflow-hidden">
-      {/* imagem */}
+    <section className="relative min-h-screen overflow-hidden">
+      {/* Imagem */}
       <motion.img
         src={ImagemHero}
         alt="Eventos Opera Café"
@@ -28,34 +28,34 @@ export default function HeroEventos() {
           duration: 2.5,
           ease: [0.22, 1, 0.36, 1],
         }}
-        className="
-          absolute
-          inset-0
-          w-full
-          h-full
-          object-cover
-        "
+        className="absolute inset-0 w-full h-full object-cover"
       />
 
-      {/* overlay */}
+      {/* Overlay */}
       <div className="absolute inset-0 bg-black/45 z-[1]" />
 
       <div
         className="
           relative
           z-10
-          h-full
+          min-h-screen
           max-w-[1700px]
           mx-auto
           flex
           flex-col
           justify-end
-          py-40
+          items-center
+          px-4
+          sm:px-10
+          lg:px-20
+          pb-16
+          sm:pb-20
+          lg:pb-32
+          pt-32
           text-center
-          px-6
         "
       >
-        {/* título */}
+        {/* Título */}
         <motion.h1
           initial={{ opacity: 0, y: 70 }}
           animate={{ opacity: 1, y: 0 }}
@@ -64,16 +64,23 @@ export default function HeroEventos() {
             delay: 0.3,
           }}
           className="
-           text-white text-4xl md:text-6xl leading-tight
+            text-white
+              text-[34px]
+              leading-[1.1]
+              md:text-6xl
+              leading-[1.1]
+              tracking-[-1px]
+              lg:tracking-[-3px]
           "
         >
-          Agenda Opera: Cultura,
-          <br />
-          Experiências e Encontros
-          que Inspiram
+          Agenda Opera:
+          <br className="hidden lg:block"/>
+           Cultura, Experiências
+          &
+          Encontros que Inspiram
         </motion.h1>
 
-        {/* descrição */}
+        {/* Descrição */}
         <motion.p
           initial={{ opacity: 0, y: 40 }}
           animate={{ opacity: 1, y: 0 }}
@@ -82,42 +89,59 @@ export default function HeroEventos() {
           }}
           className="
             mt-6
+            lg:mt-8
             text-white/80
-            text-[18px]
-            leading-[190%]
-            max-w-[700px]
-            mx-auto
+            text-[16px]
+            sm:text-[18px]
+            lg:text-[20px]
+            leading-[180%]
+            lg:leading-[190%]
+            max-w-[720px]
           "
         >
-          Descubra os próximos eventos, workshops e experiências que <br /> preparamos para você no coração do Opera Café Bistrô.
+          Descubra os próximos eventos, workshops e experiências que
+          <br className="hidden md:block" />
+          preparamos para você no coração do Opera Café Bistrô.
         </motion.p>
 
-        {/* botões */}
+        {/* Botões */}
         <motion.div
           initial="hidden"
           animate="show"
           className="
+            mt-10
+            lg:mt-12
             flex
             flex-col
             sm:flex-row
-            mx-auto
             gap-4
-            mt-[42px]
+            w-full
+            sm:w-auto
           "
         >
-          <motion.button
-              variants={buttonAnim}
-              className="bg-[#015642] hover:bg-[#017459] border-[#017459] border-3 transition px-[30px] py-[20px] rounded-full text-white cursor-pointer"
-            >
-              Explore Nossos Sabores
-            </motion.button>
+          
 
-            <motion.button
-              variants={buttonAnim}
-              className="border border-[#017459] hover:bg-[#017459] border-3 transition px-8 py-4 rounded-full text-white cursor-pointer"
-            >
-              Garanta Seu Lugar
-            </motion.button>
+          <motion.button
+            variants={buttonAnim}
+            className="
+              w-full
+              sm:w-auto
+              px-8
+              lg:px-10
+              py-4
+              lg:py-5
+              rounded-full
+              border-2
+              border-[#017459]
+              hover:bg-[#017459]
+              transition
+              text-white
+              font-medium
+              cursor-pointer
+            "
+          >
+            Reserve sua Vaga
+          </motion.button>
         </motion.div>
       </div>
     </section>
